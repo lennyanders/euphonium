7f167c10d90d92464207598afbb90842d6c84a56
5onderling <lenny.anders.98@gmail.com>
2021-08-23 23:03:33 +0200

    implement vite-plugin-checker for more type safety

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import preact from '@preact/preset-vite';
+import checker from 'vite-plugin-checker';
 
 export default defineConfig({
-  plugins: [vanillaExtractPlugin(), preact()],
+  plugins: [vanillaExtractPlugin(), preact(), checker({ typescript: true })],
 });
