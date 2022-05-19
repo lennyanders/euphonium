ce0dcb44ab2748df6f28891ac37cab7903d766d9
LennyAnders <hey@lenny.fyi>
2022-05-19 22:45:48 +0200

    make router work on history navigation

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,9 @@
 import { $, useComputed } from 'voby';
 
 const path = $(location.pathname);
+let updateUrl = false;
+
+addEventListener('popstate', () => path(location.pathname));
 
 export const Router = ({
   routes,
@@ -10,9 +13,12 @@ export const Router = ({
   return useComputed(() => {
     const p = path();
     const comp = routes.find((route) => route.path === p || route.path === '*')?.component;
-    const url = new URL(location.href);
-    url.pathname = p;
-    history.pushState(null, '', url);
+    if (updateUrl) {
+      const url = new URL(location.href);
+      url.pathname = p;
+      history.pushState(null, '', url);
+      updateUrl = false;
+    }
     return comp;
   });
 };
@@ -23,6 +29,7 @@ export const RouterLink = (props: JSX.AnchorHTMLAttributes<HTMLAnchorElement>) =
     if (target.origin !== location.origin) return;
 
     event.preventDefault();
+    updateUrl = true;
     path(target.pathname);
   };
   return <a {...props}></a>;
