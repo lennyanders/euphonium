253cc5d50f0f16b99d1cc9dba8f0fb14601e76a1
LennyAnders <hey@lenny.fyi>
2022-05-24 18:59:33 +0200

    album cover in album view for desktop and mobile

--- src/pages/Album.tsx
+++ src/pages/Album.tsx
@@ -1,7 +1,64 @@
-import { useComputed } from 'voby';
+import { $, useComputed } from 'voby';
 import { TracksList } from '../components/TracksList';
 import { params, RouterLink } from '../router';
 import { library } from '../stores/library';
+
+const AlbumInfo =
+  ({ album }: { album: FEAlbum }) =>
+  () => {
+    const desktopMatcher = matchMedia('(min-width: 640px)');
+    const desktop = $(desktopMatcher.matches);
+    desktopMatcher.addEventListener('change', ({ matches }) => desktop(matches));
+
+    if (desktop())
+      return (
+        <div class='flex gap-4'>
+          {album.cover ? (
+            <img class='w-25% rd-2' src={album.cover} />
+          ) : (
+            <div class='w-25% color-[#111] i-mdi-disk'></div>
+          )}
+          <div class='flex flex-col justify-end items-start p-b-4 overflow-hidden'>
+            <span class='p-x-1 bg-[#000D] rd-1 max-w-100% truncate relative'>{album.title}</span>
+            <small class='p-x-1 bg-[#000D] rd-1 max-w-100% truncate relative m-t-1'>
+              <RouterLink href={`/artist/${album.artist}`}>{album.artist}</RouterLink>
+            </small>
+            <small class='p-x-1 bg-[#000D] rd-1 max-w-100% truncate relative m-t-1'>
+              {[album.year, album.tracks?.length, album.diskCount, album.durationFormatted]
+                .filter((v) => v)
+                .join(' | ')}
+            </small>
+          </div>
+        </div>
+      );
+
+    const fullImage = $(false);
+    return (
+      <div
+        class={() =>
+          `${
+            fullImage() ? 'h-100vw' : 'h-[calc(200vw/3)]'
+          } relative flex flex-col justify-end items-start p-3 m--4 min-w-[calc(100%+2rem)] overflow-hidden transition-all m-b-2`
+        }
+        onClick={() => fullImage((val) => !val)}
+      >
+        {album.cover ? (
+          <img class='absolute w-100% left-0 top-50% translate-y--50%' src={album.cover} />
+        ) : (
+          <div class='absolute w-100% left-0 top-50% translate-y--50% h-100vw color-[#111] i-mdi-disk'></div>
+        )}
+        <span class='p-x-1 bg-[#000D] rd-1 max-w-100% truncate relative'>{album.title}</span>
+        <small class='p-x-1 bg-[#000D] rd-1 max-w-100% truncate relative m-t-1'>
+          <RouterLink href={`/artist/${album.artist}`}>{album.artist}</RouterLink>
+        </small>
+        <small class='p-x-1 bg-[#000D] rd-1 max-w-100% truncate relative m-t-1'>
+          {[album.year, album.tracks?.length, album.diskCount, album.durationFormatted]
+            .filter((v) => v)
+            .join(' | ')}
+        </small>
+      </div>
+    );
+  };
 
 export const Album = () => {
   const { artist, year, albumTitle } = params();
@@ -12,39 +69,30 @@ export const Album = () => {
     const album = albums.find(
       (a) => a.artist === artist && a.title === albumTitle && (year ? a.year === +year : !a.year),
     );
+    if (!album) return {};
     return {
-      ...album,
+      album,
       tracks: album?.tracks.map((id) => tracks.find((track) => track.id === id)!),
     };
   });
 
-  const { tracks, diskCount, durationFormatted } = albumData();
+  const { album, tracks } = albumData();
+  if (!album) {
+    return (
+      <p>
+        You don't have that album in you'r library, add directories in the{' '}
+        <RouterLink href='/settings' class='underline'>
+          settings
+        </RouterLink>{' '}
+        and start listening to music!
+      </p>
+    );
+  }
 
   return (
     <>
-      <h1>{albumTitle}</h1>
-      <RouterLink class='m-t--2' href={`/artist/${artist}`}>
-        {artist}
-      </RouterLink>
-      {!tracks?.length ? (
-        <p>
-          You don't have that album in you'r library, add directories in the{' '}
-          <RouterLink href='/settings' class='underline'>
-            settings
-          </RouterLink>{' '}
-          and start listening to music!
-        </p>
-      ) : (
-        <>
-          <div class='flex flex-wrap gap-2 m-t--2 m-b-2'>
-            {year && <span class='p-x-2 min-w-6 rd-4 bg-[#111] text-center'>{year}</span>}
-            <span class='p-x-2 min-w-6 rd-4 bg-[#111] text-center'>{tracks.length}</span>
-            <span class='p-x-2 min-w-6 rd-4 bg-[#111] text-center'>{diskCount}</span>
-            <span class='p-x-2 min-w-6 rd-4 bg-[#111] text-center'>{durationFormatted}</span>
-          </div>
-          <TracksList tracks={tracks} displayNumber displayDiskNumber />
-        </>
-      )}
+      <AlbumInfo album={album} />
+      <TracksList tracks={tracks} displayNumber displayDiskNumber />
     </>
   );
 };
