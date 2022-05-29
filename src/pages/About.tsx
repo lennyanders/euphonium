b43cd59f9c54a2b1f99a04e3a4b9a51915f6a064
LennyAnders <hey@lenny.fyi>
2022-05-29 16:10:26 +0200

    Add Acknowledgements to about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,7 +6,7 @@ export const About = () => (
     <h1>About</h1>
     <img src='/icon.svg' width='1' height='1' class='w-24' />
     <span>Current version {version}</span>
-    <p>Euphonium is an open source in browser music player for you'r local music files</p>
+    <p>Euphonium is an open source in browser music player for your local music files</p>
     <ul class='grid gap-2'>
       <li>
         <a href='https://github.com/lennyanders/euphonium' class='flex items-center gap-2'>
@@ -19,6 +19,36 @@ export const About = () => (
           <div class='i-mdi-file-document-outline' />
           Privacy Policy
         </RouterLink>
+      </li>
+    </ul>
+    <h2>Acknowledgements</h2>
+    <ul class='grid gap-2'>
+      <li>
+        <a href='https://snaeplayer.com/about'>
+          <span class='flex items-center gap-2'>
+            <div>
+              <div class='i-mdi-link' />
+            </div>
+            Snae Player
+          </span>
+          <small class='block p-l-6'>
+            A huge inspiration and project that made me realize what is possible
+          </small>
+        </a>
+      </li>
+      <li>
+        <a href='https://powerampapp.com/'>
+          <span class='flex items-center gap-2'>
+            <div>
+              <div class='i-mdi-link' />
+            </div>
+            PowerAmp
+          </span>
+          <small class='block p-l-6'>
+            The best music player for android and sadly only android, so that I wrote this for all
+            the other Systems
+          </small>
+        </a>
       </li>
     </ul>
   </>
