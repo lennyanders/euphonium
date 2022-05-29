0694096ae9eb5f99be5ca20964ca4c77739f0c16
LennyAnders <hey@lenny.fyi>
2022-05-29 22:26:34 +0200

    hide small player on player page - make finer dom updates on small player

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,6 @@
-import { RouterLink } from '../../router';
+import { path$, RouterLink } from '../../router';
 import { currentTrack$, pause, play, playing$ } from '../../modules/player';
+import { If } from 'voby';
 
 const MiniPlayer = () => () => {
   const track = currentTrack$();
@@ -24,7 +25,18 @@ export const Footer = () => {
       class='fixed right-2 max-w-[calc(100%-1rem)] flex items-center bottom-4 bg-[#111]'
       onTouchStart={(e) => e.stopImmediatePropagation()}
     >
-      <MiniPlayer />
+      <If when={() => path$() !== '/player' && currentTrack$()}>
+        <div class='w-64 flex items-center'>
+          <button
+            onClick={() => (playing$() ? pause() : play())}
+            class={() => ['m-4 w-8 h-8', playing$() ? 'i-mdi-pause' : 'i-mdi-play']}
+          />
+          <RouterLink href='/player' class='flex-1'>
+            <span class='truncate'>{() => currentTrack$()?.title}</span>
+            <small class='truncate block'>{() => currentTrack$()?.artist}</small>
+          </RouterLink>
+        </div>
+      </If>
       <RouterLink href='/'>
         <div class='m-4 w-8 h-8 i-mdi-view-grid-outline'></div>
       </RouterLink>
