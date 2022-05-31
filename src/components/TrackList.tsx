edc8aac494c11587b70463b9a9101f3321b2ab18
LennyAnders <hey@lenny.fyi>
2022-05-31 22:44:03 +0200

    make all tracks "play" buttons

--- src/components/TrackList.tsx
+++ src/components/TrackList.tsx
@@ -10,20 +10,22 @@ const Track = ({
   track: FETrack;
   displayNumber?: boolean;
 }) => (
-  <li
-    class={[
-      'flex gap-2 items-center p-1 rd-1',
-      () => currentTrackId$() === track.id && 'bg-[#333]',
-    ]}
-    onClick={() => play(track, tracks)}
-  >
-    {displayNumber && <span class='w-2ch text-center shrink-0'>{track.number || '-'}</span>}
-    <CoverImage src={track.cover} class='w-12 h-12 rd-1 shrink-0' />
-    <div class='break-all'>
-      {track.title}
-      <small class='block'>{track.artist}</small>
-    </div>
-    <span class='m-l-a'>{track.durationFormatted}</span>
+  <li>
+    <button
+      class={[
+        'w-100% flex gap-2 items-center p-1 rd-1',
+        () => currentTrackId$() === track.id && 'bg-[#333]',
+      ]}
+      onClick={() => play(track, tracks)}
+    >
+      {displayNumber && <span class='w-2ch text-center shrink-0'>{track.number || '-'}</span>}
+      <CoverImage src={track.cover} class='w-12 h-12 rd-1 shrink-0' />
+      <div class='break-all'>
+        {track.title}
+        <small class='block'>{track.artist}</small>
+      </div>
+      <span class='m-l-a'>{track.durationFormatted}</span>
+    </button>
   </li>
 );
 
