c4a51b2bfdc7ca3c5668b7ff80edc1a84448b885
LennyAnders <hey@lenny.fyi>
2022-05-31 22:19:55 +0200

    add lato web font

--- uno.config.ts
+++ uno.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from 'unocss';
 import presetUno from '@unocss/preset-uno';
+import presetFonts from '@unocss/preset-web-fonts';
 import presetIcons from '@unocss/preset-icons';
 
 export default defineConfig({
-  presets: [presetUno(), presetIcons()],
+  presets: [
+    presetUno(),
+    presetFonts({
+      provider: 'none',
+      fonts: {
+        sans: 'Lato',
+        lato: [{ name: 'Lato', weights: [100, 300, 400, 700, 900], italic: true }],
+      },
+    }),
+    presetIcons(),
+  ],
   rules: [
     [/^mask-size-(.*)$/, ([, d]) => ({ 'mask-size': d, '-webkit-mask-size': d })],
     [/^mask-position-(.*)$/, ([, d]) => ({ 'mask-position': d, '-webkit-mask-position': d })],
