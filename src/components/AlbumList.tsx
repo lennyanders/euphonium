acebae1ed3179f79f9515e871c86b8b1f19553be
LennyAnders <hey@lenny.fyi>
2022-06-09 23:30:04 +0200

    small css optimization

--- src/components/AlbumList.tsx
+++ src/components/AlbumList.tsx
@@ -16,18 +16,16 @@ export const AlbumList = ({ albums }: { albums: FEAlbum[] }) => (
   >
     {(album) => (
       <RouterLink
-        class='flex flex-col justify-end items-start bg-[#1c1c1c] p-1 rd-2 overflow-hidden aspect-1 text-sm relative'
+        class='bg-[#1c1c1c] text-sm relative'
         href={() =>
           `/artist/${encodeURIComponent(album().artist)}/${encodeURIComponent(album().title)}`
         }
       >
-        <CoverImage src={() => album().cover!} css='absolute top-0 left-0 w-100% h-100%' />
-        <span class='p-x-1 bg-[#000D] rd-1 max-w-100% truncate relative'>
-          {() => album().title}
-        </span>
-        <small class='p-x-1 bg-[#000D] rd-1 max-w-100% truncate relative m-t-1'>
-          {() => album().artist}
-        </small>
+        <CoverImage src={() => album().cover!} css='w-100% rd-2' />
+        <div class='absolute left-1 right-1 bottom-1 grid gap-1 justify-items-start'>
+          <span class='p-x-1 bg-[#000D] rd-1 max-w-100% truncate'>{() => album().title}</span>
+          <small class='p-x-1 bg-[#000D] rd-1 max-w-100% truncate'>{() => album().artist}</small>
+        </div>
       </RouterLink>
     )}
   </VirtualGrid>
