3ca7cab6ee3c74c17c1092ff7e2e6b16da3f67b3
LennyAnders <hey@lenny.fyi>
2022-06-16 14:53:11 +0200

    fedeout inactive navigation links on desktop

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,54 +1,47 @@
-import { RouterLink } from '../router';
+import { For } from 'voby';
+import { path$, RouterLink } from '../router';
+
+type IconLink = { href: string; icon: string; title: string };
+
+const mainLinks: IconLink[] = [
+  { href: '/tracks', icon: 'i-mdi-music-note', title: 'Tracks' },
+  { href: '/albums', icon: 'i-mdi-disk', title: 'Albums' },
+  { href: '/artists', icon: 'i-mdi-account-multiple-outline', title: 'Artists' },
+  { href: '/album-artists', icon: 'i-mdi-account-music-outline', title: 'Album Artists' },
+  { href: '/settings', icon: 'i-mdi-cog-outline', title: 'Settings' },
+];
+
+const secondaryLinks: IconLink[] = [
+  { href: '/about', icon: 'i-mdi-information-outline', title: 'About' },
+  { href: '/privacy', icon: 'i-mdi-shield-half-full', title: 'Privacy Policy' },
+];
+
+const LinkList = ({ links }: { links: IconLink[] }) => (
+  <For values={links}>
+    {({ href, icon, title }) => (
+      <li>
+        <RouterLink
+          class={[
+            'flex items-center gap-2 transition-opacity',
+            () => path$() !== href && 'lg:op-66',
+          ]}
+          href={href}
+        >
+          <div class={icon} />
+          {title}
+        </RouterLink>
+      </li>
+    )}
+  </For>
+);
 
 export const Home = () => (
   <>
     <ul class='grid gap-2'>
-      <li>
-        <RouterLink class='flex items-center gap-2' href='/tracks'>
-          <div class='i-mdi-music-note' />
-          Tracks
-        </RouterLink>
-      </li>
-      <li>
-        <RouterLink class='flex items-center gap-2' href='/albums'>
-          <div class='i-mdi-disk' />
-          Albums
-        </RouterLink>
-      </li>
-      <li>
-        <RouterLink class='flex items-center gap-2' href='/artists'>
-          <div class='i-mdi-account-multiple-outline' />
-          Artists
-        </RouterLink>
-      </li>
-      <li>
-        <RouterLink class='flex items-center gap-2' href='/album-artists'>
-          <div class='i-mdi-account-music-outline' />
-          Album Artists
-        </RouterLink>
-      </li>
-      {/* <RouterLink class='flex items-center gap-2' href='/playlists'>Playlists</RouterLink> */}
-      {/* <RouterLink class='flex items-center gap-2' href='/folders'>Folders</RouterLink> */}
-      <li>
-        <RouterLink class='flex items-center gap-2' href='/settings'>
-          <div class='i-mdi-cog-outline' />
-          Settings
-        </RouterLink>
-      </li>
+      <LinkList links={mainLinks} />
     </ul>
     <ul class='grid gap-2 m-t-a p-t-12 text-sm op-75'>
-      <li>
-        <RouterLink class='flex items-center gap-2' href='/about'>
-          <div class='i-mdi-information-outline' />
-          About
-        </RouterLink>
-      </li>
-      <li>
-        <RouterLink class='flex items-center gap-2' href='/privacy'>
-          <div class='i-mdi-shield-half-full' />
-          Privacy Policy
-        </RouterLink>
-      </li>
+      <LinkList links={secondaryLinks} />
     </ul>
   </>
 );
