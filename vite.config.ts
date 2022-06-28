5364e01ebb2a09b26e44afc6176d14d659e00c11
LennyAnders <hey@lenny.fyi>
2022-06-28 21:27:43 +0200

    remove not really use vite type checker plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import unocss from 'unocss/vite';
 import voby from 'voby-vite';
-import checker from 'vite-plugin-checker';
 
 export default defineConfig({
-  plugins: [voby(), unocss(), checker({ typescript: true })],
+  plugins: [voby(), unocss()],
 });
