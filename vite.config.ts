d82b13889ef24cca81c04d5a04e9bb8115074359
LennyAnders <hey@lenny.fyi>
2022-09-02 17:41:49 +0200

    update depencies and handle dots "." in urls

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import unocss from 'unocss/vite';
 import voby from 'voby-vite';
+import rewriteAll from 'vite-plugin-rewrite-all';
 
 export default defineConfig({
-  plugins: [voby(), unocss()],
+  plugins: [voby(), unocss(), rewriteAll()],
 });
