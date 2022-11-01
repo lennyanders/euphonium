3db9673371584be256628dff29e2fb5160fd631f
LennyAnders <hey@lenny.fyi>
2022-11-01 01:22:33 +0100

    drastically improve image downscaling quality - takes a bit more time, but it's worth the extra quality

--- src/worker/files/getOptimizedImage.ts
+++ src/worker/files/getOptimizedImage.ts
@@ -1,15 +1,10 @@
 export const getOptimizedImage = async (image: Blob, resolution: number) => {
-  const imageBitmap = await createImageBitmap(image, {
-    resizeHeight: resolution,
-    resizeWidth: resolution,
-  });
+  const imageBitmap = await createImageBitmap(image);
   const canvas = new OffscreenCanvas(resolution, resolution);
 
   const context = canvas.getContext('2d')!;
   // @ts-ignore
-  context.imageSmoothingEnabled = true;
-  // @ts-ignore
   context.imageSmoothingQuality = 'high';
-  context.drawImage(imageBitmap, 0, 0);
-  return await canvas.convertToBlob({ type: 'image/webp', quality: 1 });
+  context.drawImage(imageBitmap, 0, 0, resolution, resolution);
+  return await canvas.convertToBlob({ type: 'image/webp', quality: 0.8 });
 };
