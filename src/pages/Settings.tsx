f541c64f2101d52cac6b97376ec0ed28f0547039
LennyAnders <hey@lenny.fyi>
2022-12-01 00:01:46 +0100

    fix button styles for disabled buttons

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -50,7 +50,7 @@ onMessage(async ({ data }) => {
 
 export const Settings = () => {
   // prettier-ignore
-  const buttonCss = 'bg-dark-500 p-1-2 rd-1 hover:bg-dark-800 transition-background-color transition-color';
+  const buttonCss = 'bg-dark-500 p-1-2 rd-1 enabled:hover:bg-dark-800 transition-background-color transition-color';
   const importing$ = useMemo(() => !!state.importing);
   return [
     <h1>Settings</h1>,
