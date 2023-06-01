7828649cfbc8dab5862c2e940ecd36c7ab466721
LennyAnders <hey@lenny.fyi>
2023-06-01 21:12:15 +0200

    update packages and add voby-vite (hmr YEAH)

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,15 @@ import unocss from 'unocss/vite';
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
 import rewriteAll from 'vite-plugin-rewrite-all';
+import voby from 'voby-vite';
 
 export default defineConfig({
-  plugins: [unocss(), checker({ typescript: true }), rewriteAll()],
-  esbuild: {
-    jsxInject: `import { h as $$h, Fragment as $$F } from 'voby';\n`,
-    jsxFactory: '$$h',
-    jsxFragment: '$$F',
-  },
+  plugins: [
+    unocss(),
+    voby({ hmr: { enabled: process.env.NODE_ENV !== 'production', filter: /\.(jsx|tsx)$/ } }),
+    checker({ typescript: true }),
+    rewriteAll(),
+  ],
   define: {
     'global.Uint8Array': 'Uint8Array',
     'process.env.NODE_DEBUG': false,
