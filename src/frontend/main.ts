cba4c3be2140d357fd8fcded42cb3089d7ee4858
LennyAnders <hey@lenny.fyi>
2023-11-20 23:54:14 +0100

    set title for pages

--- src/frontend/main.ts
+++ src/frontend/main.ts
@@ -11,30 +11,40 @@ import Albums from './pages/Albums.vue';
 import Artists from './pages/Artists.vue';
 import Tracks from './pages/Tracks.vue';
 
+const getTitle = (title: string) => `${title} | ${document.title}`;
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
       path: '/tracks',
       component: Tracks,
+      meta: { title: getTitle('Tracks') },
     },
     {
       path: '/albums',
       component: Albums,
+      meta: { title: getTitle('Albums') },
     },
     {
       path: '/artists',
       component: Artists,
+      meta: { title: getTitle('Artists') },
     },
     {
       path: '/album-artists',
       component: AlbumArtists,
+      meta: { title: getTitle('Album artists') },
     },
     {
       path: '/:pathMatch(.*)*',
       redirect: '/tracks',
     },
   ],
+});
+
+router.afterEach((route) => {
+  if (typeof route.meta.title === 'string') document.title = route.meta.title;
 });
 
 createApp(App).use(router).mount(document.body);
